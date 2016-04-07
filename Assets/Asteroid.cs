3735299d73d4f8238f48b339a6a552bb664ced14
regisclaus <regis.claus@gmail.com>
2016-04-07 11:56:10 -0300

    Asteroids
    - Criado 5 formatos diferentes de asteroids
    - Tudo na pasta prefab asteroids
    - Para alterar o tamanho, só mexer no scale
    - Quando o robô acerta ele, por enquanto manda mensagem de fim de jogo

---
+++ Assets/Asteroid.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "Robot") {
+			GameObject.Find ("GameManager").SendMessage ("endGame");
+		}
+	}
+}
