9239799f3f122b129371aaacb970012ef89001c6
Douglas Eduardo Parra <douglas.eduardop@gmail.com>
2016-05-09 18:14:34 -0300

    Field Chave
    - Campo chave + porta + parede

---
+++ Assets/FieldChave.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldChave : MonoBehaviour {
+
+    private GameObject robot;
+    public GameObject parede;
+
+    // Use this for initialization
+    void Start()
+    {
+        robot = GameObject.Find("Robot");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator AutomaticShot()
+    {
+        yield return new WaitForSeconds(.1f);
+        robot.SendMessage("launch");
+        //		yield return new WaitForSeconds(.1f);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Robot")
+        {
+            parede.SetActive(false);
+            StartCoroutine(AutomaticShot());
+        }
+    }
+
+}
